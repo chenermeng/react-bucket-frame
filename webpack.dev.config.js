61107b130e201f22b6d2bcaf969505e31c9a65d5
chenermeng <1165706028@qq.com>
2018-06-13 22:24:13 +0800

    update: 添加配置

---
+++ webpack.dev.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+module.exports = {
+    // 入口
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        path: path.join(__dirname, 'dist/'),
+        filename: 'bundle.js'
+    }
+};
